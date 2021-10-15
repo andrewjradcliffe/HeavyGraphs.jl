073a1899f2fa0c9f52037214099698358d07785a
Andrew Radcliffe <andrew.radcliffe@intel.com>
2021-10-15 10:19:41 -0700

    Change variable name for clarity

--- src/countabsent.jl
+++ src/countabsent.jl
@@ -9,9 +9,9 @@ function countabsent!(f::Function, dest::Tuple{Vararg{Array{T}} T},
                       t::AbstractNode, N::Int, C::Int, levs_ks::Vector{Vector{Any}})
     Ñ = N - 1
     if C == Ñ
-        ks = setdiff(levs_ks[C], keys(t))
+        lev_ks = setdiff(levs_ks[C], keys(t))
         ν = 1
-        isempty(ks) || f(dest, ks, ν)
+        isempty(ks) || f(dest, lev_ks, ν)
     elseif C < Ñ
         ν = multabsentupto(t, N, C, levs_ks)
         iszero(ν) || f(dest, levs_ks[Ñ], ν)
@@ -23,9 +23,9 @@ function countabsent!(f::Function, fs::Vector{Function}, dest::Tuple{Vararg{Arra
                       t::AbstractNode, N::Int, C::Int, levs_ks::Vector{Vector{Any}})
     Ñ = N - 1
     if C == Ñ
-        ks = filter!(fs[C], setdiff(levs_ks[C], keys(t)))
+        lev_ks = filter!(fs[C], setdiff(levs_ks[C], keys(t)))
         ν = 1
-        isempty(ks) || f(dest, ks, ν)
+        isempty(ks) || f(dest, lev_ks, ν)
     elseif C < Ñ
         ν = multabsentupto(fs, t, N, C, levs_ks)
         iszero(ν) || f(dest, filter(fs[Ñ], levs_ks[Ñ]), ν)
