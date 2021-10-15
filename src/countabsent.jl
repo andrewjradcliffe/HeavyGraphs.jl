506bb11aec674bee6d69c334bd42c0f6dddd79b9
Andrew Radcliffe <andrew.radcliffe@intel.com>
2021-10-15 11:24:02 -0700

    Fix countabsent variable rename

--- src/countabsent.jl
+++ src/countabsent.jl
@@ -11,7 +11,7 @@ function countabsent!(f::Function, dest::Tuple{Vararg{Array{T}} T},
     if C == Ñ
         lev_ks = setdiff(levs_ks[C], keys(t))
         ν = 1
-        isempty(ks) || f(dest, lev_ks, ν)
+        isempty(lev_ks) || f(dest, lev_ks, ν)
     elseif C < Ñ
         ν = multabsentupto(t, N, C, levs_ks)
         iszero(ν) || f(dest, levs_ks[Ñ], ν)
@@ -25,7 +25,7 @@ function countabsent!(f::Function, fs::Vector{Function}, dest::Tuple{Vararg{Arra
     if C == Ñ
         lev_ks = filter!(fs[C], setdiff(levs_ks[C], keys(t)))
         ν = 1
-        isempty(ks) || f(dest, lev_ks, ν)
+        isempty(lev_ks) || f(dest, lev_ks, ν)
     elseif C < Ñ
         ν = multabsentupto(fs, t, N, C, levs_ks)
         iszero(ν) || f(dest, filter(fs[Ñ], levs_ks[Ñ]), ν)
