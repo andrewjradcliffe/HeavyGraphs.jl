09362f7d4e7927db979f03cd6f31927dc390a5b2
Andrew Radcliffe <andrew.radcliffe@intel.com>
2021-10-26 14:32:57 -0700

    Add tests of AbstractGraph; load of data file

--- src/performance.jl
+++ src/performance.jl
@@ -28,19 +28,25 @@ pni6 = PathKeys([IndexedPathKey(i) for i = 1:10]);
 pm6 = PathKeys([[IndexedPathKey(i) for i = 1:9]; IndexedPathKey(string, 10)]);
 
 gf() = SimpleNode()
+sdg() = SimpleDiGraph()
+sg() = SimpleGraph()
 #### SimpleNode vs. AryNode
 # Result: growth. SimpleNode seems to be moderately faster
 @benchmark grow!(gf, SimpleNode(), pni3, eachcol(qmat3)) seconds=30
 @benchmark grow!(gf, SimpleNode(), pm3, eachcol(qmat3)) seconds=30
 @benchmark grow!(af, AryNode(), pni3, eachcol(qmat3)) seconds=30
 @benchmark grow!(af, AryNode(), pm3, eachcol(qmat3)) seconds=30
+@benchmark grow!(sdg, SimpleDiGraph(), pni3, eachcol(qmat3)) seconds=30
 
 t0 = grow!(gf, SimpleNode(), pni, eachcol(mat));
 a0 = grow!(af, AryNode(), pni, eachcol(mat));
+g0 = grow!(sdg, SimpleDiGraph(), pni, eachcol(mat));
 t3 = grow!(gf, SimpleNode(), pni3, eachcol(qmat3));
 a3 = grow!(af, AryNode(), pni3, eachcol(qmat3));
+g3 = grow!(sdg, SimpleDiGraph(), pni3, eachcol(qmat3));
 t6 = grow!(gf, SimpleNode(), pni6, eachcol(qmat6));
 a6 = grow!(af, AryNode(), pni6, eachcol(qmat6));
+g6 = grow!(sdg, SimpleDiGraph(), pni6, eachcol(qmat6));
 
 # Result: traversal tests.
 # 4x50000: AryNode is faster overall
@@ -74,17 +80,39 @@ fl3_v2!(dest, x) = (dest[1][1] += length(unique(p -> p.second.val[1], x)))
 fl3_v3!(dest, x) = (dest[1][1] += length(unique!([p.second.val[1] for p in x])))
 @benchmark mapat(fl!, ((5,),), t3, 10)
 @benchmark mapat(fl!, ((5,),), a3, 10)
+@benchmark mapat(fl!, ((5,),), g3, 10)
+
+################
+prefix = "/nfs/site/home/aradclif/my_cit_scratch1/diagnosis/1276/x76se/gt_g1m/2021-09-15";
+pni7 = PathKeys([IndexedPathKey(i) for i = 1:10]);
+suffix8 = "D117E4C0_36.arrow" # 218MiB
+file8 = joinpath(prefix, suffix8);
+dmat8 = permutedims(hcat([convert(Vector{Any}, copy(col)) for col in Arrow.Table(file8)]...));
+####
+#### Comparison given optimized Arrow files
+cellinst(x) = @inbounds (x[1], pathprefix(x[2]))
+prefix10 = "/nfs/site/home/aradclif/my_cit_scratch1/diagnosis/1276/x76se/gt_g1m/2021-09-24";
+suffix10 = "D117E4C0_36.arrow";
+file10 = joinpath(prefix10, suffix10);
+dmat10 = permutedims(hcat([convert(Vector{Any}, copy(col)) for col in Arrow.Table(file10)]...));
+pni10 = PathKeys([IndexedPathKey(x -> Tuple(x), [1, 2]); [IndexedPathKey(i) for i = 3:8]])
+vi10 = IndexedPathKey(cellinst, [10, 9])
+####
 
 # Result: growth on heterogeneous keys. SimpleNode is faster
 @benchmark grow!(gf, SimpleNode(), pni7, eachcol(dmat8)) seconds=240
 @benchmark grow!(af, AryNode(), pni7, eachcol(dmat8)) seconds=240
+@benchmark grow!(sdg, SimpleDiGraph(), pni7, eachcol(dmat8)) seconds=240
 @benchmark valgrow!(gf, SimpleNode(), vi10, pni10, eachcol(dmat10)) seconds=240
 @benchmark valgrow!(af, AryNode(), vi10, pni10, eachcol(dmat10)) seconds=240
+@benchmark datagrow!(sdg, SimpleDiGraph(), vi10, pni10, eachcol(dmat10)) seconds=240
 
 t7 = grow!(gf, SimpleNode(), pni7, eachcol(dmat8));
 a7 = grow!(af, AryNode(), pni7, eachcol(dmat8));
+g7 = grow!(sdg, SimpleDiGraph(), pni7, eachcol(dmat8));
 t10 = valgrow!(gf, SimpleNode(), vi10, pni10, eachcol(dmat10));
 a10 = valgrow!(af, AryNode(), vi10, pni10, eachcol(dmat10));
+g10 = datagrow!(sdg, SimpleDiGraph(), vi10, pni10, eachcol(dmat10));
 
 # Result: traversal tests
 # t7: 4387231 nodes. AryNode is moderately faster
