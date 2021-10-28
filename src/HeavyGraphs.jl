862a428b0ff3727fe408285817253bba7fa58ef2
Andrew Radcliffe <andrew.radcliffe@intel.com>
2021-10-28 15:58:11 -0700

    Rearrange export list

--- src/HeavyGraphs.jl
+++ src/HeavyGraphs.jl
@@ -3,34 +3,9 @@ module HeavyGraphs
 using EqualRanges
 using SetDiffCard
 
-# map.jl
-export mapat, mapat!, tmapat,
-    mapfilterat, mapfilterat!, tmapfilterat,
-    mapupto, mapupto!, tmapupto,
-    mapfilterupto, mapfilterupto!, tmapfilterupto
-
-# kmap.jl
-export kmapat, kmapat!, tkmapat,
-    kmapfilterat, kmapfilterat!, tkmapfilterat,
-    kmapupto, kmapupto!, tkmapupto,
-    kmapfilterupto, kmapfilterupto!, tkmapfilterupto
-
-# multipliers.jl
-export multabsentupto, multabsent
-
-# iterators.jl
-export foreachat, foreachfilterat, foreach_depthfirst, foreach_breadthfirst,
-    foreachfrom, foreachthrough, foreachupto,
-    countat, countall, countfrom, countthrough, countupto,
-    findpathsat, findpathsat!, findpathsall, findpathsall!,
-    findpathsfrom, findpathsfrom!, findpathsthrough, findpathsthrough!, findpathsupto, findpathsupto!
-
 # abstractpathkeys.jl
 export AbstractPathKey, AbstractIndexedPathKey, IndexedPathKey,
     AbstractPathKeys, PathKeys
-
-# countabsent.jl
-export countabsent!, countstatus!
 
 # abstractgraph.jl
 export AbstractGraph, AbstractSimpleDiGraph, AbstractSimpleGraph,
@@ -44,12 +19,39 @@ export get_datapush!
 
 export grow!, grow, datagrow!, datagrow, tdatagrow!
 
+# map.jl
+export mapat, mapat!, tmapat,
+    mapfilterat, mapfilterat!, tmapfilterat,
+    mapupto, mapupto!, tmapupto,
+    mapfilterupto, mapfilterupto!, tmapfilterupto
+
+# kmap.jl
+export kmapat, kmapat!, tkmapat,
+    kmapfilterat, kmapfilterat!, tkmapfilterat,
+    kmapupto, kmapupto!, tkmapupto,
+    kmapfilterupto, kmapfilterupto!, tkmapfilterupto
+
+# iterators.jl
+export foreachat, foreachfilterat, foreach_depthfirst, foreach_breadthfirst,
+    foreachfrom, foreachthrough, foreachupto,
+    countat, countall, countfrom, countthrough, countupto,
+    findpathsat, findpathsat!, findpathsall, findpathsall!,
+    findpathsfrom, findpathsfrom!, findpathsthrough, findpathsthrough!, findpathsupto, findpathsupto!
+
+# multipliers.jl
+export multabsentupto, multabsent
+
+# countabsent.jl
+export countabsent!, countstatus!
+
+include("abstractpathkeys.jl")
+include("abstractgraph.jl")
+
 include("map.jl")
 include("kmap.jl")
-include("multipliers.jl")
 include("iterators.jl")
-include("abstractpathkeys.jl")
+
+include("multipliers.jl")
 include("countabsent.jl")
-include("abstractgraph.jl")
 
 end
