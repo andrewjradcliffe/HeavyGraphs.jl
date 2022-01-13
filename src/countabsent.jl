a4c80371bc43c80e04b09f765ea2bcc042b4b79b
Andrew Radcliffe <andrew.radcliffe@intel.com>
2022-01-13 14:50:58 -0800

    Update where {T} statements for ndadd!, ndadd1!

--- src/countabsent.jl
+++ src/countabsent.jl
@@ -107,49 +107,49 @@ end
 # (dest, ks, x) -> kcountstatus!(f, fs, dest[1], ks, x)
 # with `f` something such as: x -> getindex(x, 1)
 ################
-function ndadd!(fs::Vector{Function}, A::Array{T, N}, v::T, ks::Vararg{Any, N}) where {N} where {T}
+function ndadd!(fs::Vector{Function}, A::Array{T, N}, v::T, ks::Vararg{Any, N}) where {N} where {T<:Real}
     # idxs::NTuple{N, Int} = ntuple(i -> fs[i](ks[i]), Val(N))
     # A[idxs...] += v
     idxs = ntuple(i -> fs[i](ks[i]), N)
     ndadd!(A, v, idxs)
     return A
 end
-function ndadd!(fs::Vector{Function}, A::Array{T, N}, v::T, ks::Vector) where {N} where {T}
+function ndadd!(fs::Vector{Function}, A::Array{T, N}, v::T, ks::Vector) where {N} where {T<:Real}
     idxs = ntuple(i -> fs[i](ks[i]), N)
     ndadd!(A, v, idxs)
     return A
 end
 
-function ndadd1!(fs::Vector{Function}, A::Array{T, N}, ks::Vararg{Any, N}) where {N} where {T}
+function ndadd1!(fs::Vector{Function}, A::Array{T, N}, ks::Vararg{Any, N}) where {N} where {T<:Real}
     idxs = ntuple(i -> fs[i](ks[i]), N)
     ndadd1!(A, idxs)
     return A
 end
-function ndadd1!(fs::Vector{Function}, A::Array{T, N}, ks::Vector) where {N} where {T}
+function ndadd1!(fs::Vector{Function}, A::Array{T, N}, ks::Vector) where {N} where {T<:Real}
     idxs = ntuple(i -> fs[i](ks[i]), N)
     ndadd1!(A, idxs)
     return A
 end
 
-@generated function ndadd!(A::Array{T, N}, v::T, idxs::NTuple{N, Int}) where {N} where {T}
+@generated function ndadd!(A::Array{T, N}, v::T, idxs::NTuple{N, Int}) where {N} where {T<:Real}
     quote
         Base.Cartesian.@nextract $N i d -> idxs[d]
         (Base.Cartesian.@nref $N A i) += v
         return A
     end
 end
-function ndadd!(A::Array{T, N}, v::T, idxs::Vararg{S, N}) where {N} where {T} where {S<:Integer}
+function ndadd!(A::Array{T, N}, v::T, idxs::Vararg{S, N}) where {N} where {T<:Real} where {S<:Integer}
     ndadd!(A, v, idxs)
 end
 
-@generated function ndadd1!(A::Array{T, N}, idxs::NTuple{N, Int}) where {N} where {T}
+@generated function ndadd1!(A::Array{T, N}, idxs::NTuple{N, Int}) where {N} where {T<:Real}
     quote
         Base.Cartesian.@nextract $N i d -> idxs[d]
         (Base.Cartesian.@nref $N A i) += one(T)
         return A
     end
 end
-function ndadd1!(A::Array{T, N}, idxs::Vararg{S, N}) where {N} where {T} where {S<:Integer}
+function ndadd1!(A::Array{T, N}, idxs::Vararg{S, N}) where {N} where {T<:Real} where {S<:Integer}
     ndadd1!(A, idxs)
 end
 
