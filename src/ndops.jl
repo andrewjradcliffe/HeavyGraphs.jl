b6135f6123af8ac69a03b9580e2efa5108d2320e
Andrew Radcliffe <andrew.radcliffe@intel.com>
2022-01-13 15:25:57 -0800

    Add ndpush!

--- src/ndops.jl
+++ src/ndops.jl
@@ -69,3 +69,55 @@ end
 function ndadd1!(A::Array{T, N}, idxs::Vararg{S, N}) where {N} where {T<:Real} where {S<:Integer}
     ndadd1!(A, idxs)
 end
+
+
+#### 2022-01-13: p. 639: push! strategy
+
+function ndpush!(fs::Vector{Function}, A::Array{Vector{Vector{T}}, N}, v::Vector{T},
+                      ks::Vararg{S, N}) where {N} where {T} where {S}
+    idxs = ntuple(i -> fs[i](ks[i]), N)
+    ndpush!(A, v, idxs)
+    return A
+end
+
+function ndpush!(fs::Vector{Function}, A::Array{Vector{Vector{T}}, N}, v::Vector{T},
+                      ks::Vararg{Any, N}) where {N} where {T}
+    idxs = ntuple(i -> fs[i](ks[i]), N)
+    ndpush!(A, v, idxs)
+    return A
+end
+
+function ndpush!(fs::Vector{Function}, A::Array{Vector{Vector{T}}, N}, v::Vector{T},
+                      ks::Vector) where {N} where {T}
+    idxs = ntuple(i -> fs[i](ks[i]), N)
+    ndpush!(A, v, idxs)
+    return A
+end
+
+@generated function ndpush!(A::Array{Vector{Vector{T}}, N}, v::Vector{T},
+                                 idxs::NTuple{N, Int}) where {N} where {T}
+    quote
+        Base.Cartesian.@nextract $N i d -> idxs[d]
+        push!((Base.Cartesian.@nref $N A i), v)
+        return A
+    end
+end
+
+function ndpush!(A::Array{Vector{Vector{T}}, N}, v::Vector{T},
+                      idxs::Vararg{S, N}) where {N} where {T} where {S<:Integer}
+    ndpush!(A, v, idxs)
+end
+
+vv = [1,2,3]
+@code_warntype ndpush!(𝓃𝓂idxs, vv, (1, 2))
+fs = [x -> parse(Int, x), x -> 2]
+ks = ["1", 3]
+@code_warntype ndpush!(fs, 𝓃𝓂idxs, vv, ks...)
+@code_warntype ndpush!(fs, 𝓃𝓂idxs, vv, ks)
+
+A = zeros(Int, 2, 2);
+@benchmark ndadd1!(fs, A, ks)
+
+@benchmark ndadd1!(A, (1, 2))
+
+@benchmark ndadd1_v2!(fs, A, ks)
