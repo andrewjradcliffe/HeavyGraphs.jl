de8340a866a0c22e24f826c4da189c9756941a7c
Andrew Radcliffe <andrew.radcliffe@intel.com>
2022-01-13 15:23:46 -0800

    Finally arrive at an ndadd!, ndadd1! that are non-allocating

--- src/ndops.jl
+++ src/ndops.jl
@@ -8,28 +8,44 @@
 # N-dimensional operations; origin is ndadd!, and also ndpush!.
 # Eventually, this might moved to a separate package, along with other indexing items.
 ################
-function ndadd!(fs::Vector{Function}, A::Array{T, N}, v::T, ks::Vararg{Any, N}) where {N} where {T<:Real}
-    # idxs::NTuple{N, Int} = ntuple(i -> fs[i](ks[i]), Val(N))
-    # A[idxs...] += v
-    idxs = ntuple(i -> fs[i](ks[i]), N)
-    ndadd!(A, v, idxs)
-    return A
+@generated function ndadd!(fs::Vector{Function}, A::Array{T, N}, v::T,
+                           ks::Vararg{Any, N}) where {N} where {T<:Real}
+    quote
+        Base.Cartesian.@ncall $N ndadd! A v i -> fs[i](ks[i])
+        return A
+    end
 end
-function ndadd!(fs::Vector{Function}, A::Array{T, N}, v::T, ks::Vector) where {N} where {T<:Real}
-    idxs = ntuple(i -> fs[i](ks[i]), N)
-    ndadd!(A, v, idxs)
-    return A
+@generated function ndadd!(fs::Vector{Function}, A::Array{T, N}, v::T,
+                           ks::Vector) where {N} where {T<:Real}
+    quote
+        Base.Cartesian.@ncall $N ndadd! A v i -> fs[i](ks[i])
+        return A
+    end
 end
 
-function ndadd1!(fs::Vector{Function}, A::Array{T, N}, ks::Vararg{Any, N}) where {N} where {T<:Real}
-    idxs = ntuple(i -> fs[i](ks[i]), N)
-    ndadd1!(A, idxs)
-    return A
+@generated function ndadd1!(fs::Vector{Function}, A::Array{T, N},
+                            ks::Vararg{Any, N}) where {N} where {T<:Real}
+    quote
+        Base.Cartesian.@ncall $N ndadd1! A i -> fs[i](ks[i])
+        return A
+    end
 end
-function ndadd1!(fs::Vector{Function}, A::Array{T, N}, ks::Vector) where {N} where {T<:Real}
-    idxs = ntuple(i -> fs[i](ks[i]), N)
-    ndadd1!(A, idxs)
-    return A
+@generated function ndadd1!(fs::Vector{Function}, A::Array{T, N}, ks::Vector) where {T<:Real, N}
+    # # Version 1
+    # quote
+    #     Base.Cartesian.@nexprs $N i -> i_i = fs[i](ks[i])
+    #     (Base.Cartesian.@nref $N A i) += one(T)
+    #     return A
+    # end
+    # # Version 2
+    # quote
+    #     ndadd1!(A, (Base.Cartesian.@ntuple $N i -> fs[i](ks[i])))
+    # end
+    # # Version 3: success
+    quote
+        Base.Cartesian.@ncall $N ndadd1! A i -> fs[i](ks[i])
+        return A
+    end
 end
 
 @generated function ndadd!(A::Array{T, N}, v::T, idxs::NTuple{N, Int}) where {N} where {T<:Real}
@@ -53,13 +69,3 @@ end
 function ndadd1!(A::Array{T, N}, idxs::Vararg{S, N}) where {N} where {T<:Real} where {S<:Integer}
     ndadd1!(A, idxs)
 end
-
-
-@generated function ndadd1_v2!(fs::Vector{Function}, A::Array{T, N}, ks::Vector) where {N} where {T<:Real}
-    quote
-        Base.Cartesian.@nexprs $N i -> i_i = fs[i](ks[i])
-        (Base.Cartesian.@nref $N A i) += one(T)
-        return A
-    end
-end
-
